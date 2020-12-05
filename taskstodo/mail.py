acb7ca13e524c1f76207b239cd4c5d7bdc247e92
sruthi-vaskuri <68856653+sruthi-vaskuri@users.noreply.github.com>
2020-12-06 00:32:17 +0530

    Update mail.py

--- taskstodo/mail.py
+++ taskstodo/mail.py
@@ -5,12 +5,14 @@ def sendEmail(to,content):
     server = smtplib.SMTP('smtp.gmail.com', 587)
     server.ehlo()
     server.starttls()
+    ## Automatic login by the server
     server.login('sruthisrinivas0@gmail.com', 'Vaskuri@s981')
     server.sendmail('sruthisrinivas0@gmail.com', to, content)
     server.close()
 
 def sendmailto(mailto,mailcontent):
     try:
+        ## sending mail to the given mail ID along with the content
         emailid=mailto   
         sendEmail(emailid,mailcontent)
         mailmsg = "Email Sent"
